5a7c2e1c596961d6fc20d321b7e62e45fbb88eef
pfisterm <pfisterm@users.noreply.github.com>
2022-03-23 18:14:02 -0400

    create function my_usd to format prices and remove duplicate

--- app/groceries.py
+++ app/groceries.py
@@ -7,6 +7,17 @@
 
 import os
 
+
+def to_usd(my_price):
+    """
+    This functions formats numbers to follow conventional US currency formatting.
+    The parameters of this function is my_price which is a float.
+
+    Invoke like this: to_usd(9.99)
+    """
+    return '${:,.2f}'.format(my_price)
+
+
 # checks to see if a products.csv file exists. If not, it uses the default
 if os.path.isfile(os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")) == True:
     print("USING CUSTOM PRODUCTS CSV FILE...")
@@ -33,7 +44,7 @@ print("THERE ARE", len(products), "PRODUCTS:")
 print("---------")
 
 for p in products:
-    print("..." + p["name"] + "   " + '${:,.2f}'.format(p["price"]))
+    print("..." + p["name"] + "   " + to_usd(p["price"]))
 
 
 all_prices = []
@@ -44,7 +55,7 @@ import statistics
 avg_price = statistics.median(all_prices)
 
 print("---------")
-print("AVERAGE PRICE:", '${:,.2f}'.format(avg_price))
+print("AVERAGE PRICE:", to_usd(avg_price))
 
 
 
