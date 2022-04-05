db7587855f2ce9ae00e52afb398bcec42f103570
pfisterm <pfisterm@users.noreply.github.com>
2022-04-05 15:05:20 -0400

    Update stocks.py

--- app/stocks.py
+++ app/stocks.py
@@ -13,7 +13,6 @@ load_dotenv()
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
 
 symbol = input("Please input a ticker symbol (default: 'NFLX'): ") or "NFLX"
-url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
 
 df = read_csv(url)
 #print(df.columns)
